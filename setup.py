c97fed241ca0e8247878450662a60d972e040905
Viktor Stískala <viktor@stiskala.cz>
2013-09-09 13:48:22 +0200

    Fixed topics

--- setup.py
+++ setup.py
@@ -32,6 +32,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Python Modules',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
